fde2d94f2b04ff5a57c37e34bdbd8eec2361c4b4
Michael Barnathan <michael@barnathan.name>
2015-12-20 13:09:21 -0500

    Add experimental device queue.

--- synchronized-dimming.groovy
+++ synchronized-dimming.groovy
@@ -46,6 +46,8 @@ def updated() {
 def initialize() {
     unsubscribe()
     subscribeTo(dimmers ?: [])
+    atomicState.deviceQueue = [:]
+    atomicState.rootDevice = ""
 }
 
 private subscribeTo(devices) {
@@ -60,14 +62,42 @@ def tieLevel(event) {
     if (level == 0 && event.value != "0") {
         log.warn "Non-numeric dimmer level coming from ${event.dump()}: ${event.value}"
     } else {
-        // To avoid pingponging events back and forth forever.
-        def remainingDimmers = (dimmers ?: []).findAll { (it.currentValue("level") as int) != level }
-        log.trace "${remainingDimmers.size()} dimmers remain to be updated to level ${level}"
+        /* To avoid pingponging events forever:
+             1. Wait for levels to settle before propagating events from children.
+             2. No-op if the level to set is identical to the current level.
+         */
+        def deviceQueue = atomicState.deviceQueue
+
+        // Because SmartThings prohibits ConcurrentHashMap.
+        synchronized(this) {
+            if (deviceQueue.remove(event.deviceId) == level) {
+                log.trace "${event.displayName} has settled at level ${level}"
+
+                if (!deviceQueue) {
+                    log.trace "Synchronized to ${atomicState.rootDevice}: all dimmers have settled."
+                    atomicState.rootDevice = ""
+                }
+                return
+            } else if (!deviceQueue) {
+                // With an empty queue, this should be the initiating device.
+                atomicState.rootDevice = event.displayName
+                sendNotificationEvent("${event.displayName} set to ${level}, synchronizing ${dimmers.size() - 1} dimmers.")
+            }
+        }
+
+        def remainingDimmers = (dimmers ?: []).findAll { (it != event.device && it.currentValue("level") as int) != level }
+        if (remainingDimmers) {
+            synchronized(this) {
+                deviceQueue << remainingDimmers.collectEntries { ["${it.device.id}": level] }
+            }
+            log.trace "${remainingDimmers.size()} dimmers remain to be updated to level ${level}, immediate trigger: ${event.displayName}, root trigger: ${atomicState.rootDevice}."
+        }
         remainingDimmers*.setLevel(level)
     }
 }
 
 def tieOn(event) {
+    sendNotificationEvent("${event.displayName} on, turning on ${dimmers.size()} dimmers")
     dimmers?.on()
 }
 
